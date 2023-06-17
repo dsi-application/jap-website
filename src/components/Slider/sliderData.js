2b5678642966f01f37fbc76e54609ebf9aadc197
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-06-17 11:38:30 +0100

    adding taher's translated components and adding some images to slider

--- src/components/Slider/sliderData.js
+++ src/components/Slider/sliderData.js
@@ -4,44 +4,22 @@ const slider = [
         title: "",
         description: "",
         
-        image: "./slide3.png",
+        image: "./header.png",
     },
-
     {
         title: "",
         description: "",
         
-        image: "./slide2.png",
+        image: "./beach.jpg",
     },
-
     {
         title: "",
         description: "",
         
-        image: "./slide4.png",
+        image: "./TEQ07514.jpg",
     },
 
-    {
-        title: "",
-        description: "",
-        
-        image: "./SIMULATION-2-_1920x900.jpg",
-
-    },
-
-    {
-        title: "",
-        description: "",
-        
-        image: "./SIMULATION-4-_1920x900.jpg",
-    },
-
-    {
-        title: "",
-        description: "",
-        
-        image: "./SIMULATION-7-.png",
-    }
+ 
 
    
    
