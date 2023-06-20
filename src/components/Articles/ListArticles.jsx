bc4df18a69b04ef7aaec7e4419e83692b365763a
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-06-20 01:00:49 +0100

    some changes

--- src/components/Articles/ListArticles.jsx
+++ src/components/Articles/ListArticles.jsx
@@ -1,42 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import articlesData from './articles.json';
-import { Card, Col, Container , Row } from 'react-bootstrap';
-import './ListArticles.css'; 
-import i18n from '../../i18n';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import './ListArticles.css';
 import { Link } from 'react-router-dom';
-
-
+import { useTranslation } from 'react-i18next';
 
 function ListArticles() {
-  //console.log(i18n.language);
-  const [filtred,setFiltred]=React.useState([]);
-  //const [data,setData]=React.useState([]);
+  const { i18n } = useTranslation();
+  const [filteredArticles, setFilteredArticles] = useState([]);
+
   useEffect(() => {
-    
-    const data = articlesData.filter((article) => article.lng === i18n.language);
-    setFiltred(data);
-    console.log(i18n);
-    }, [i18n]);
-  //const filtred = articlesData.filter((article) => article.lng === i18n.language);
+    const filteredData = articlesData.filter((article) => article.lng === i18n.language);
+    setFilteredArticles(filteredData);
+  }, [i18n.language]);
 
   return (
-    <Container style={{height:'100%'}} className='mt-2'>
-    <Row md={12}>
-    {filtred.map((article) => (
-      <Col md={4} xs={12} sm={12}>
-      <Card className='card-animation' key={article.id} style={{ width: '100%',margin:"15px" }}>
-        <Card.Img variant="top" src={require(`./images${article.photo}`).default} alt={article.titre} />
-        <Card.Body>
-          <Card.Title>{article.titre}</Card.Title>
-          <Card.Text>{article.description}</Card.Text>
-          <Link to={`/article/${article.id}`} className="btn btn-primary">Read More</Link>
-        </Card.Body>
-      </Card>
-        </Col>
-  
-    ))}
-    </Row>
-  </Container>
+    <Container style={{ height: '100%' }} className='mt-2'>
+      <Row md={12}>
+        {filteredArticles.map((article) => (
+          <Col md={4} xs={12} sm={12} key={article.id}>
+            <div className='card-container'>
+              <Card className='card-animation mb-4' >
+                <div className='card-image-container'>
+                  <Card.Img
+                    variant='top'
+                    src={require(`./images${article.photo}`).default}
+                    alt={article.titre}
+                  />
+                </div>
+                <Card.Body>
+                  <Card.Title>{article.titre}</Card.Title>
+                  <Card.Text>{article.description}</Card.Text>
+                  <Link to={`/article/${article.id}`} className='btn btn-primary'>
+                    Read More
+                  </Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
